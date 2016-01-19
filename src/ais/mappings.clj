56c9976a51db78533aebdc7d6d8e5668bb96c5ad
Navil Charles <navil.charles@gmail.com>
2016-01-19 00:36:13 -0500

    Added type 21 support  * Verified manually but integration test outstanding.

--- src/ais/mappings.clj
+++ src/ais/mappings.clj
@@ -146,6 +146,30 @@
   {:len   4 :desc "Spare"                    :tag "spare"        :fn ais-types/x}
 ))
 
+(def mapping-21 (list
+  {:len   2 :desc "Repeat Indicator"         :tag "repeat"       :fn ais-types/u}
+  {:len  30 :desc "MMSI"                     :tag "mmsi"         :fn ais-types/u}
+  {:len   5 :desc "Aid type"                 :tag "aid_type"     :fn (partial ais-types/e ais-vocab/nav-aid-type)}
+  {:len 120 :desc "Name"                     :tag "name"         :fn (partial ais-types/t ais-vocab/sixbit-ascii 20)}
+  {:len   1 :desc "Position Accuracy"        :tag "accuracy"     :fn ais-types/b}
+  {:len  28 :desc "Longtitude"               :tag "lon"          :fn (partial ais-types/I (/ 1.0 600000) 4)}
+  {:len  27 :desc "Latitude"                 :tag "lat"          :fn (partial ais-types/I (/ 1.0 600000) 4)}
+  {:len   9 :desc "Dimension to Bow"         :tag "to_bow"       :fn ais-types/u}
+  {:len   9 :desc "Dimension to Stern"       :tag "to_stern"     :fn ais-types/u}
+  {:len   6 :desc "Dimension to Port"        :tag "to_port"      :fn ais-types/u}
+  {:len   6 :desc "Dimension to Starboard"   :tag "to_starboard" :fn ais-types/u}
+  {:len   4 :desc "Type of EPFD"             :tag "epfd"         :fn (partial ais-types/e ais-vocab/position-fix-type)}
+  {:len   6 :desc "UTC second"               :tag "second"       :fn ais-types/u}
+  {:len   1 :desc "Off-Position Indicator"   :tag "off_position" :fn ais-types/b}
+  {:len   8 :desc "Regional reserved"        :tag "regional"     :fn ais-types/u}
+  {:len   1 :desc "RAIM flag"                :tag "raim"         :fn ais-types/b}
+  {:len   1 :desc "Virtual-aid flag"         :tag "virtual_aid"  :fn ais-types/u}
+  {:len   1 :desc "Assigned mode flag"       :tag "assigned"     :fn ais-types/u}
+  {:len   1 :desc "Spare"                    :tag "spare"        :fn ais-types/x}
+  {:len  88 :desc "Name"                     :tag "name_ext"     :fn (partial ais-types/t ais-vocab/sixbit-ascii 14)}
+))
+
+
 
 
 (def msg-spec {
@@ -156,4 +180,5 @@
   5 mapping-5
  18 mapping-18
  19 mapping-19
+ 21 mapping-21
 })
