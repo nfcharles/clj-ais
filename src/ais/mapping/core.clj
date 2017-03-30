9f92bc439fb7a0af5ea80d7a0f15702e4567114c
Navil Charles <navil.charles@gmail.com>
2017-03-30 17:06:07 -0400

    Implement type 13

--- src/ais/mapping/core.clj
+++ src/ais/mapping/core.clj
@@ -14,6 +14,7 @@
             [ais.mapping.type_10 :as type_10]
             [ais.mapping.type_11 :as type_11]
             [ais.mapping.type_12 :as type_12]
+            [ais.mapping.type_13 :as type_13]
             [ais.mapping.type_14 :as type_14]
             [ais.mapping.type_18 :as type_18]
             [ais.mapping.type_19 :as type_19]
@@ -67,6 +68,7 @@
    10 type_10/mapping-10
    11 type_11/mapping-11
    12 type_12/mapping-12
+   13 type_13/mapping-13
    14 type_14/mapping-14
    18 type_18/mapping-18
    19 type_19/mapping-19
---
+++ src/ais/mapping/type_13.clj
@@ -0,0 +1,25 @@
+(ns ais.mapping.type_13
+  (:require [ais.types :as ais-types]
+            [ais.mapping.common :as common])
+  (:gen-class))
+
+
+(def mmsi-seq-fields (list
+  {:len 30 :desc "MMSI"     :tag "mmsi" :fn ais-types/u}
+  {:len  2 :desc "Sequence" :tag "seq"  :fn ais-types/u}
+))
+
+(defn field-mapper [_]
+  mmsi-seq-fields)
+
+(def bits-len (partial ais-types/array-bit-len 4 32))
+
+(def seq-handler (partial ais-types/a 32 field-mapper common/parse-binary))
+
+(def mapping-13 (list
+  {:len        6 :desc "Message Type"     :tag "type"   :fn (partial common/const 13)}
+  {:len        2 :desc "Repeat Indicator" :tag "repeat" :fn ais-types/u}
+  {:len       30 :desc "Source MMSI"      :tag "mmsi"   :fn ais-types/u}
+  {:len        2 :desc "Spare"            :tag "spare"  :fn ais-types/x}
+  {:len bits-len :desc "MMSI's"           :tag "mmsis"  :fn seq-handler :a true}
+))
