1a8e8698ea30168ab148294692c1ef1f1779fd13
Navil Charles <navil.charles@gmail.com>
2017-06-26 11:24:02 -0400

    Arg parse updates in decode  * removed extraneous imports

--- src/ais/decode.clj
+++ src/ais/decode.clj
@@ -1,37 +1,40 @@
 (ns ais.decode
-  (:require [ais.util :as ais-util]
-            [ais.extractors :as ais-ex]
-            [ais.vocab :as ais-vocab]
-            [ais.mapping.core :as ais-map]
-            [ais.core :as ais-core]
-            [ais.runner :as ais-runner]
-            [clojure.core.async :as async]
-            [clojure.data.csv :as csv]
-            [clojure.pprint :as pprint]
-            [clojure.stacktrace :as strace]
-            [clj-json.core :as json]
+  (:require [clojure.core.async :as async]
             [taoensso.timbre :as logging]
-            [taoensso.timbre.appenders.core :as appenders])
+            [ais.runner :as ais-runner])
   (:gen-class))
 
 (def stdin-reader
   (java.io.BufferedReader. *in*))
 
+
+(defn input-stream [reader]
+  (async/to-chan (line-seq reader)))
+
+(defn parse-output-prefix [args]
+  (nth args 0))
+
+(defn parse-decode-types [args]
+  (ais-runner/parse-types (nth args 1)))
+
+(defn parse-thread-count [args]
+  (read-string (nth args 2)))
+
+(defn parse-output-format [args]
+  (nth args 3))
+
+(defn parse-write-buffer-length [args]
+  (read-string (nth args 4)))
+
 (defn -main
   [& args]
   (try
-    (let [out-prefix (nth args 0)
-          include-types (ais-runner/parse-types (nth args 1))
-          n-threads (read-string (nth args 2))
-          out-format (nth args 3)
-          buffer-len (read-string (nth args 4))
-          stream (async/to-chan (line-seq stdin-reader))]
-      (ais-runner/configure-logging :std-err)
-      (time (ais-runner/run stream
-                            out-prefix
-                            include-types
-                            n-threads
-                            out-format
-                            buffer-len)))
+    (ais-runner/configure-logging :std-err)
+    (time (ais-runner/run (input-stream stdin-reader)
+                          (parse-output-prefix args)
+                          (parse-decode-types args)
+                          (parse-thread-count args)
+                          (parse-output-format args)
+                          (parse-write-buffer-length args)))
     (catch Exception e
       (logging/fatal e))))
